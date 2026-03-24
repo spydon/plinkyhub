b08e19454cef1a8d2b4e82888438af16d041f20f
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-03-24 23:24:55 +0100

    Update favicon to brighter teal arc icon

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4037575255" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "769018335" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
