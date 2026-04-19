de44b663c75193430f5c40a5c679ada7a38a5e2c
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-04-19 17:23:24 +0200

    Hide dump tab, show firmware list directly without tab bar

--- lib/pages/firmware/firmware_page.dart
+++ lib/pages/firmware/firmware_page.dart
@@ -1,100 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
-import 'package:plinkyhub/pages/firmware/dump_tab.dart';
 import 'package:plinkyhub/pages/firmware/firmware_list_tab.dart';
-import 'package:plinkyhub/routing/routes.dart';
 
-enum FirmwareTab {
-  firmware,
-  dump,
-}
-
-class FirmwarePage extends ConsumerStatefulWidget {
+class FirmwarePage extends ConsumerWidget {
   const FirmwarePage({this.initialTab, super.key});
 
   final String? initialTab;
 
   @override
-  ConsumerState<FirmwarePage> createState() => _FirmwarePageState();
-}
-
-class _FirmwarePageState extends ConsumerState<FirmwarePage>
-    with SingleTickerProviderStateMixin {
-  late final TabController _tabController;
-
-  @override
-  void initState() {
-    super.initState();
-    final initialIndex = widget.initialTab != null
-        ? FirmwareTab.values
-              .firstWhere(
-                (tab) => tab.name == widget.initialTab,
-                orElse: () => FirmwareTab.firmware,
-              )
-              .index
-        : 0;
-
-    _tabController = TabController(
-      length: FirmwareTab.values.length,
-      vsync: this,
-      initialIndex: initialIndex,
-    );
-    _tabController.addListener(_handleTabChange);
-  }
-
-  @override
-  void didUpdateWidget(FirmwarePage oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (widget.initialTab != null &&
-        widget.initialTab != oldWidget.initialTab) {
-      final tab = FirmwareTab.values.firstWhere(
-        (entry) => entry.name == widget.initialTab,
-        orElse: () => FirmwareTab.firmware,
-      );
-      if (_tabController.index != tab.index) {
-        _tabController.animateTo(tab.index);
-      }
-    }
-  }
-
-  void _handleTabChange() {
-    if (!_tabController.indexIsChanging) {
-      final tabName = FirmwareTab.values[_tabController.index].name;
-      // Use a query parameter instead of a path segment to avoid
-      // colliding with the firmware detail route (/firmware/:name).
-      context.go('${AppRoute.firmware.path}?tab=$tabName');
-    }
-  }
-
-  @override
-  void dispose() {
-    _tabController.removeListener(_handleTabChange);
-    _tabController.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TabBar(
-          controller: _tabController,
-          tabs: const [
-            Tab(text: 'Firmware'),
-            Tab(text: 'Dump'),
-          ],
-        ),
-        Expanded(
-          child: TabBarView(
-            controller: _tabController,
-            children: const [
-              FirmwareListTab(),
-              DumpTab(),
-            ],
-          ),
-        ),
-      ],
-    );
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const FirmwareListTab();
   }
 }
